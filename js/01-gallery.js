1d064abfd555d224b6d9835acfa46c4d9eb8f210
Mykkolla <tarabukin.n.10@gmail.com>
2023-03-01 16:51:43 +0200

    add:

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// add ItemsImg
+const galleryItemsTable = galleryItems.map(({preview , original, description}) => 
+`<a class="gallery__link" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>`
+).join('');
+
+
+const galleryMain = document.querySelector('.gallery');
+
+galleryMain.insertAdjacentHTML('beforeend', galleryItemsTable);
+
+const hendlerClickOnImg = (e) => {
+// preventDefault
+    e.preventDefault();
+// checkOnClick
+if(!e.target.classList.contains('gallery__image')) {
+    return;
+}
+// change src
+e.target.src = e.target.dataset.source;
+// modal
+const imageSrc = e.target.src;
+const imageAlt = e.target.alt;
+
+const lightbox = basicLightbox.create(`
+      <img src="${imageSrc}" alt="${imageAlt}">
+    `);  
+lightbox.show();
+
+// close modal
+ const hendlerCloseModalEsc = (e) => {
+    if(e.code === "Escape") {
+lightbox.close()
+    }
+ } 
+ window.addEventListener('keydown', hendlerCloseModalEsc)
+};
+
+
+galleryMain.addEventListener('click', hendlerClickOnImg)
+
+
+
