7ebce13719973f3ae113e792466eb7972c3543de
Mykkolla <tarabukin.n.10@gmail.com>
2023-03-03 20:00:01 +0200

    add:

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,6 +2,7 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 
+
 const galleryItemsTable = galleryItems.map(({preview , original, description}) => 
 `<a class="gallery__item" href="${original}">
 <img class="gallery__image" src="${preview}" alt="${description}" />
