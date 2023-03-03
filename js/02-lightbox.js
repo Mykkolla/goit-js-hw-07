4454cd036d1e7cd15f76c5d41dbd8246a1706d16
Mykkolla <tarabukin.n.10@gmail.com>
2023-03-03 19:22:41 +0200

    add:

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryItemsTable = galleryItems.map(({preview , original, description}) => 
+`<a class="gallery__item" href="${original}">
+<img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+).join('');
+
+
+const galleryMain = document.querySelector('.gallery');
+
+galleryMain.insertAdjacentHTML('beforeend', galleryItemsTable);
+
+
+const hendlerClickOnImg = (e) => {
+    // preventDefault
+        e.preventDefault();}
+
+ galleryMain.addEventListener('click', hendlerClickOnImg)
+
+ let gallery = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250});
+
+
+
